d766de9c8bb983ebcc3193a6eb6ed7571d7fcd99
nikhilyanamndra87 <60419334+nikhilyanamndra87@users.noreply.github.com>
2021-12-03 01:52:39 +0530

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -24,8 +24,8 @@ def classify(a):
     filename = 'model_pickle'
     model_reloaded = pickle.load(open(filename, 'rb'))
     
-    te =[]
-    te.append(a)
+    text=[]
+    text.append(a)
     ab = model_reloaded.predict_proba(te)
     np.set_printoptions(formatter={'float_kind':'{:f}'.format})
     result = ab.tolist()
@@ -36,9 +36,9 @@ def classify(a):
             ,"Reduced Inequalites","Sustainable Cities and Communities",
             "Responsible Consumption and Production","Climate Action","Life Below Water","Life On Land"]
     t =zip(li_goals,test_res)
-    df_predic = pd.DataFrame(t,columns=["SDG","Probability"])
-    df_predic.index = df_predic.index + 1
-    return((df_predic))
+    df_prediction = pd.DataFrame(t,columns=["SDG","Probability"])
+    df_prediction.index = df_predic.index + 1
+    return((df_prediction))
     
 
 
@@ -55,7 +55,7 @@ with body:
         'Choose your .txt file', type="txt")
     if uploaded_file is not None:
         rawtext = str(uploaded_file.read(), 'utf-8')
-    if st.button('Get Results'):
+    if st.button('classify'):
         with classify_container:
             if rawtext == "":
                 st.header('Results')
